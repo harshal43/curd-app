25fdf33e69025de8f95b8aa1066a5422facd2ef0
Harshal <harshal88843bh@gmail.com>
2023-12-27 23:33:38 +0530

    inital git

---
+++ src/app/core/core.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoreService {
+
+  constructor(
+    private _snackBar: MatSnackBar
+  ) { }
+
+  openSnackBar(msg:string,action:string='ok') {
+    this._snackBar.open(msg,action, {
+      duration:2000,
+      verticalPosition:'top',
+    });
+  }
+}
